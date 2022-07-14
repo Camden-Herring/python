a4b7dbe22cba55f103ea4c60bfe79c1399e5bb5d
Camden-Herring <camdenherring1103@gmail.com>
2022-07-14 09:13:27 -0400

    ...

--- flask/fundamentals/html_table/static/bs.py
+++ flask/fundamentals/html_table/static/bs.py
@@ -1,3 +1,14 @@
 bootstrap = {
     "table" : "table table-dark text-center"
 }
+
+
+# use this in the header 
+"""
+<link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
+"""
+
+# use this just before the end of the body
+"""
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+"""
